84da009f41e2563bda748bc556b7e6340b1327ef
Oana Ghicajanu <oana.ghicajanu@smedix.com>
2023-01-06 13:33:21 +0000

    Merged PR 4542: modified cancel button and save button
    Related work items: #22334

--- RAT/src/app/dialog-device-type/dialog-device-type.component.ts
+++ RAT/src/app/dialog-device-type/dialog-device-type.component.ts
@@ -8,32 +8,28 @@ import { DeviceTypeService } from '../services/device-type.service';
   templateUrl: './dialog-device-type.component.html',
   styleUrls: ['./dialog-device-type.component.css']
 })
-export class DialogDeviceTypeComponent implements OnInit {
+export class DialogDeviceTypeComponent {
   showError: boolean = false;
   dialogStatus: string = "closed";
 
   constructor(
     public dialogRef: MatDialogRef<DialogDeviceTypeComponent>,
     @Inject(MAT_DIALOG_DATA) public data: DeviceTypes,
-    public dialogService: DeviceTypeService,
+    public deviceTypeService: DeviceTypeService,
   ) { }
-
-  ngOnInit(): void {
-    this.dialogCreate;
-  }
 
   onCancelClick(): void {
     this.dialogRef.close();
   }
 
   dialogCreate(deviceTypes: { id: number, name: string }) {
-    this.dialogService.addDeviceType(this.data).subscribe({
+    this.deviceTypeService.addDeviceType(this.data).subscribe({
       next: resp => {
         this.dialogRef.close(this.dialogStatus);
-       },
-      error: error =>
-      {console.log(error)
-       if (error.status == 700) { this.showError = true; }
+      },
+      error: error => {
+        console.log(error)
+        if (error.status == 700) { this.showError = true; }
       }
     })
   }
