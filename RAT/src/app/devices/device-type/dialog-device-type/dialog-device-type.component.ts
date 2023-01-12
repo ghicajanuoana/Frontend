a8984b282ab58a1fd106281e4bafe5052909df37
Oana Ghicajanu <oana.ghicajanu@smedix.com>
2023-01-12 09:51:19 +0000

    Merged PR 4599: added toastr for add and edit device type
    ![addError.PNG](https://smedix.visualstudio.com/9eab10cc-5d70-4301-9c8e-d24be2c89625/_apis/git/repositories/58fba364-e224-4fb6-b56f-1bb110972797/pullRequests/4599/attachments/addError.PNG) ![update.PNG](https://smedix.visualstudio.com/9eab10cc-5d70-4301-9c8e-d24be2c89625/_apis/git/repositories/58fba364-e224-4fb6-b56f-1bb110972797/pullRequests/4599/attachments/update.PNG) ![updateError.PNG](https://smedix.visualstudio.com/9eab10cc-5d70-4301-9c8e-d24be2c89625/_apis/git/repositories/58fba364-e224-4fb6-b56f-1bb110972797/pullRequests/4599/attachments/updateError.PNG) ![added.PNG](https://smedix.visualstudio.com/9eab10cc-5d70-4301-9c8e-d24be2c89625/_apis/git/repositories/58fba364-e224-4fb6-b56f-1bb110972797/pullRequests/4599/attachments/added.PNG) +added toastr for add and edit device type
    -i will add screenshots for page
    
    Related work items: #22352

--- RAT/src/app/devices/device-type/dialog-device-type/dialog-device-type.component.ts
+++ RAT/src/app/devices/device-type/dialog-device-type/dialog-device-type.component.ts
@@ -1,7 +1,8 @@
-import { Component, Inject, OnInit } from '@angular/core';
+import { Component, Inject } from '@angular/core';
 import { DeviceTypes } from '../../../models/device-type.model';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { DeviceTypeService } from '../../../services/device-type.service';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-dialog-device-type',
@@ -9,7 +10,6 @@ import { DeviceTypeService } from '../../../services/device-type.service';
   styleUrls: ['./dialog-device-type.component.css']
 })
 export class DialogDeviceTypeComponent {
-  showError: boolean = false;
   dialogStatus: string = "closed";
   deviceTypeName: string;
   deviceTypeId: any;
@@ -18,6 +18,7 @@ export class DialogDeviceTypeComponent {
     public dialogRef: MatDialogRef<DialogDeviceTypeComponent>,
     @Inject(MAT_DIALOG_DATA) public data: { deviceType: DeviceTypes, isEditMode: boolean, dialogTitle: string },
     public deviceTypeService: DeviceTypeService,
+    private toastr: ToastrService
   ) { }
 
   ngOnInit(): void {
@@ -34,11 +35,14 @@ export class DialogDeviceTypeComponent {
     if (!this.data.isEditMode) {
       this.deviceTypeService.addDeviceType(deviceTypes).subscribe({
         next: resp => {
+          this.toastr.success("Device type successfully added!");
           this.dialogRef.close(this.dialogStatus);
         },
         error: error => {
           console.log(error)
-          if (error.status == 700) { this.showError = true; }
+          if (error.status == 700) {
+            this.toastr.error("Error, device type name must be unique!");
+          }
         }
       })
     }
@@ -46,10 +50,13 @@ export class DialogDeviceTypeComponent {
       deviceTypes.deviceTypeId = this.deviceTypeId;
       this.deviceTypeService.updateDeviceType(deviceTypes).subscribe({
         next: resp => {
+          this.toastr.success("Device type successfully updated!");
           this.dialogRef.close(this.dialogStatus);
         },
         error: error => {
-          if (error.status == 409) { this.showError = true; }
+          if (error.status == 409) {
+            this.toastr.error("Error, device type name must be unique!");
+          }
         }
       })
     }
